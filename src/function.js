6221986279f761edb063ae17d465f7a16e22ab59
Benson Kiai <benson.kiai86@gmail.com>
2023-07-14 04:27:50 +0300

    Fix ESLinters errors

--- src/function.js
+++ src/function.js
@@ -1,24 +1,20 @@
 export default function populateTodoList() {
   const tasks = [
-    { description: "task1", completed: false, index: 1 },
-    { description: "task2", completed: true, index: 2 },
-    { description: "task3", completed: false, index: 3 },
+    { description: 'task1', completed: false, index: 1 },
+    { description: 'task2', completed: true, index: 2 },
+    { description: 'task3', completed: false, index: 3 },
   ];
 
-  const todoList = document.getElementById("todo");
+  const todoList = document.getElementById('todo');
 
   tasks.forEach((task) => {
-    const listItem = document.createElement("li");
-    listItem.className = "task-item";
+    const listItem = document.createElement('li');
+    listItem.className = 'task-item';
     listItem.innerHTML = `
- <div class="taskContainer">
-      <input type="checkbox" class="checkbox" ${
-        task.completed ? "checked" : ""
-      }>
-      <input type="text" class="Text" value="${task.description}" ${
-      task.completed ? "disabled" : ""
-    }>
-      <button class="removeBtn" type="button">&#x1F5D1;</button>
+ <div class='taskContainer'>
+      <input type='checkbox' class='checkbox' ${task.completed ? 'checked' : ''}>
+      <input type='text' class='Text' value='${task.description}' ${task.completed ? 'disabled' : ''}>
+      <button class='removeBtn' type='button'>&#x1F5D1;</button>
  </div>     
     `;
     todoList.appendChild(listItem);
