01cec331f5d3b023ead53829d9f9a492d7926bcc
Benson Kiai <benson.kiai86@gmail.com>
2023-07-14 12:16:55 +0300

    Fix ESlinter errors

--- src/function.js
+++ src/function.js
@@ -1,4 +1,4 @@
-import { addTask, updateLocalStorage } from "./app.js";
+import { addTask, updateLocalStorage } from './app.js';
 
 const removeTask = (tasks, taskId) => {
   if (taskId >= 0 && taskId < tasks.length) {
@@ -13,47 +13,43 @@ const clearCompletedTasks = (tasks) => {
 };
 
 export default function populateTodoList() {
-  const tasksString = localStorage.getItem("tasks");
+  const tasksString = localStorage.getItem('tasks');
   const tasks = tasksString ? JSON.parse(tasksString) : [];
-  const todoList = document.getElementById("todo");
-  todoList.innerHTML = "";
+  const todoList = document.getElementById('todo');
+  todoList.innerHTML = '';
   tasks.forEach((task, index) => {
-    const listItem = document.createElement("li");
-    listItem.className = "task-item";
+    const listItem = document.createElement('li');
+    listItem.className = 'task-item';
     listItem.innerHTML = `
-      <div class="taskContainer">
-        <input type="checkbox" class="checkbox" ${
-          task.completed ? "checked" : ""
-        }>
-        <input type="text" class="Text" value="${task.description}" ${
-      task.completed ? "disabled" : ""
-    }>
-        <button class="removeBtn" type="button" data-id="${index}">&#x1F5D1;</button>
-      </div>     
+      <div class='taskContainer'>
+        <input type='checkbox' class='checkbox' ${task.completed ? 'checked' : ''}>
+        <input type='text' class='Text' value='${task.description}' ${task.completed ? 'disabled' : ''}>
+        <button class='removeBtn' type='button' data-id='${index}'>&#x1F5D1;</button>
+      </div>
     `;
     todoList.appendChild(listItem);
   });
-  const addBtn = document.getElementById("addBtn");
-  const newTaskInput = document.getElementById("new-task-input");
-  const clearButton = document.getElementById("clear");
-  addBtn.addEventListener("click", () => {
+  const addBtn = document.getElementById('addBtn');
+  const newTaskInput = document.getElementById('new-task-input');
+  const clearButton = document.getElementById('clear');
+  addBtn.addEventListener('click', () => {
     const description = newTaskInput.value;
-    if (description.trim() !== "") {
+    if (description.trim() !== '') {
       addTask(tasks, description);
       updateLocalStorage(tasks);
       populateTodoList();
-      newTaskInput.value = "";
+      newTaskInput.value = '';
     }
   });
-  clearButton.addEventListener("click", () => {
+  clearButton.addEventListener('click', () => {
     clearCompletedTasks(tasks);
     updateLocalStorage(tasks);
     populateTodoList();
   });
-  const removeButtons = document.getElementsByClassName("removeBtn");
+  const removeButtons = document.getElementsByClassName('removeBtn');
   Array.from(removeButtons).forEach((button) => {
-    button.addEventListener("click", (event) => {
-      const taskId = event.target.getAttribute("data-id");
+    button.addEventListener('click', (event) => {
+      const taskId = event.target.getAttribute('data-id');
       removeTask(tasks, taskId);
       updateLocalStorage(tasks);
       populateTodoList();
