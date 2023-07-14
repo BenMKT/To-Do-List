85fb34948b08baa8464e7788fe192c340c657a7f
Benson Kiai <benson.kiai86@gmail.com>
2023-07-14 11:44:45 +0300

    Add app buttons events & fill in tasks dynamically

--- src/function.js
+++ src/function.js
@@ -1,22 +1,62 @@
+import { addTask, updateLocalStorage } from "./app.js";
+
+const removeTask = (tasks, taskId) => {
+  if (taskId >= 0 && taskId < tasks.length) {
+    tasks.splice(taskId, 1);
+  }
+};
+const clearCompletedTasks = (tasks) => {
+  const remainingTasks = tasks.filter((task) => !task.completed);
+  tasks.length = 0;
+  remainingTasks.forEach((task) => tasks.push(task));
+  updateLocalStorage(tasks);
+};
+
 export default function populateTodoList() {
-  const tasks = [
-    { description: 'task1', completed: false, index: 1 },
-    { description: 'task2', completed: true, index: 2 },
-    { description: 'task3', completed: false, index: 3 },
-  ];
-
-  const todoList = document.getElementById('todo');
-
-  tasks.forEach((task) => {
-    const listItem = document.createElement('li');
-    listItem.className = 'task-item';
+  const tasksString = localStorage.getItem("tasks");
+  const tasks = tasksString ? JSON.parse(tasksString) : [];
+  const todoList = document.getElementById("todo");
+  todoList.innerHTML = "";
+  tasks.forEach((task, index) => {
+    const listItem = document.createElement("li");
+    listItem.className = "task-item";
     listItem.innerHTML = `
- <div class='taskContainer'>
-      <input type='checkbox' class='checkbox' ${task.completed ? 'checked' : ''}>
-      <input type='text' class='Text' value='${task.description}' ${task.completed ? 'disabled' : ''}>
-      <button class='removeBtn' type='button'>&#x1F5D1;</button>
- </div>     
+      <div class="taskContainer">
+        <input type="checkbox" class="checkbox" ${
+          task.completed ? "checked" : ""
+        }>
+        <input type="text" class="Text" value="${task.description}" ${
+      task.completed ? "disabled" : ""
+    }>
+        <button class="removeBtn" type="button" data-id="${index}">&#x1F5D1;</button>
+      </div>     
     `;
     todoList.appendChild(listItem);
+  });
+  const addBtn = document.getElementById("addBtn");
+  const newTaskInput = document.getElementById("new-task-input");
+  const clearButton = document.getElementById("clear");
+  addBtn.addEventListener("click", () => {
+    const description = newTaskInput.value;
+    if (description.trim() !== "") {
+      addTask(tasks, description);
+      updateLocalStorage(tasks);
+      populateTodoList();
+      newTaskInput.value = "";
+    }
+  });
+  clearButton.addEventListener("click", () => {
+    clearCompletedTasks(tasks);
+    updateLocalStorage(tasks);
+    populateTodoList();
+  });
+  const removeButtons = document.getElementsByClassName("removeBtn");
+  Array.from(removeButtons).forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const taskId = event.target.getAttribute("data-id");
+      removeTask(tasks, taskId);
+      updateLocalStorage(tasks);
+      populateTodoList();
+    });
   });
 }
