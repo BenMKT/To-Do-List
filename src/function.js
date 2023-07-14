7ecb6758c789c08ea78b8663c466758dfa14a259
Benson Kiai <benson.kiai86@gmail.com>
2023-07-14 15:14:56 +0300

    Add checkbox event listener

--- src/function.js
+++ src/function.js
@@ -3,8 +3,10 @@ import { addTask, updateLocalStorage } from './app.js';
 const removeTask = (tasks, taskId) => {
   if (taskId >= 0 && taskId < tasks.length) {
     tasks.splice(taskId, 1);
+    updateLocalStorage(tasks);
   }
 };
+
 const clearCompletedTasks = (tasks) => {
   const remainingTasks = tasks.filter((task) => !task.completed);
   tasks.length = 0;
@@ -22,16 +24,30 @@ export default function populateTodoList() {
     listItem.className = 'task-item';
     listItem.innerHTML = `
       <div class='taskContainer'>
-        <input type='checkbox' class='checkbox' ${task.completed ? 'checked' : ''}>
-        <input type='text' class='Text' value='${task.description}' ${task.completed ? 'disabled' : ''}>
+        <input type='checkbox' class='checkbox' ${
+          task.completed ? 'checked' : ''
+        }>
+        <input type='text' class='Text' value='${task.description}' ${
+      task.completed ? 'disabled' : ''
+    }>
         <button class='removeBtn' type='button' data-id='${index}'>&#x1F5D1;</button>
-      </div>
+      </div>     
     `;
+
+    const checkbox = listItem.querySelector('.checkbox');
+    checkbox.addEventListener('change', () => {
+      tasks[index].completed = checkbox.checked;
+      updateLocalStorage(tasks);
+    });
+
     todoList.appendChild(listItem);
+    
   });
+
   const addBtn = document.getElementById('addBtn');
   const newTaskInput = document.getElementById('new-task-input');
   const clearButton = document.getElementById('clear');
+
   addBtn.addEventListener('click', () => {
     const description = newTaskInput.value;
     if (description.trim() !== '') {
@@ -41,11 +57,13 @@ export default function populateTodoList() {
       newTaskInput.value = '';
     }
   });
+
   clearButton.addEventListener('click', () => {
     clearCompletedTasks(tasks);
     updateLocalStorage(tasks);
     populateTodoList();
   });
+
   const removeButtons = document.getElementsByClassName('removeBtn');
   Array.from(removeButtons).forEach((button) => {
     button.addEventListener('click', (event) => {
