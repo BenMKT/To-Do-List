dce6d6b712beb4bd618fbd0db2731a7304a67b79
Benson Kiai <benson.kiai86@gmail.com>
2023-07-21 00:11:59 +0300

    Fix ESlinter errors

--- Mock/updateLocalStorage.js
+++ Mock/updateLocalStorage.js
@@ -1,11 +1,9 @@
 const updateLocalStorage = (tasks) => {
-    tasks.forEach((task, index) => {
-      task.index = index + 1;
-    });
-  
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  };
-  
-  module.exports = updateLocalStorage;
+  tasks.forEach((task, index) => {
+    task.index = index + 1;
+  });
 
-  
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+module.exports = updateLocalStorage;
